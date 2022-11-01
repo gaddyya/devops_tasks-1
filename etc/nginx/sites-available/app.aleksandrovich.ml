456fea0a848f0e069eb679ddaeb4132a911f19f5
Vadim-Aleksandrovich <alexandrovichvadim@gmail>
2022-11-01 16:14:38 +0000

    feat: task completed

---
+++ etc/nginx/sites-available/app.aleksandrovich.ml
@@ -0,0 +1,31 @@
+server {
+	server_name app.aleksandrovich.ml;
+	root /var/www/app.aleksandrovich.ml/;
+	index index.html index.htm index.nginx-debian.html;
+	location / {
+		try_files $uri $uri/ =404;
+	}
+
+    listen 443 ssl; # managed by Certbot
+    ssl_certificate /etc/letsencrypt/live/app.aleksandrovich.ml/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/app.aleksandrovich.ml/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+}
+
+
+server {
+    if ($host = app.aleksandrovich.ml) {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+
+
+	server_name app.aleksandrovich.ml;
+	listen 80;
+
+	server_name _;
+    return 404; # managed by Certbot
+
+
+}
