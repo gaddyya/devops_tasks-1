828c8018bdfc7ee4b2128dda963d82fecea5f602
Vadim Aleksandrovich <vadim.alexandrovich@protonmail.ch>
2022-11-11 00:00:36 +0300

    refactor: move task_6 solution to another branch

--- nginx_cgi_install/cgi_installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && apt get install nginx fxgiwrap build-essential -y
-mkdir /usr/lib/cgi-bin -p
-cp cpu.cgi /usr/lib/cgi-bin/cpu.cgi
-cp fcgiwrap.conf /etc/nginx/fcgiwrap.conf
-cp example_server /etc/nginx/sites-available/example_server
--- nginx_cgi_install/cpu.cgi
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CPU_UTILIZATION=`cat /proc/loadavg`
-
-echo "content-type: text/plain"
-echo "CPU utiliozation is: $CPU_UTILIZATION"
