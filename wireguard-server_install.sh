b511021c03402e9a4b79bf4e37e094bd736c767f
Vadim Aleksandrovich <vadim.alexandrovich@protonmail.ch>
2022-11-12 18:06:33 +0300

    fix: add line

--- wireguard-server_install.sh
+++ wireguard-server_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y wireguard
 wg genkey | tee /etc/wireguard/privatekey | wg pubkey | tee /etc/wireguard/publickey
 
 
