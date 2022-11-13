d5e858986dc8b843a48d37fed7180a6e078ba8cd
Vadim Aleksandrovich <vadim.alexandrovich@protonmail.ch>
2022-11-13 20:35:02 +0300

    feat: task 10 comleted

---
+++ install_awc_cli.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "This is linux-gnu"
+    OS_LIKE=`grep '^ID_LIKE' /etc/os-release`
+    PAC_YUM='fedora'
+    PAC_APT='debian'
+    if [[ "$OS_LIKE" == *"$PAC_YUM"* ]]; then
+        sudo yum update -y
+        sudo yum install awscli -y
+        aws --version
+    fi
+    if [[ "$OS_LIKE" == *"$PAC_APT"* ]]; then
+        sudo apt-get update -y
+        sudo apt-get install awscli -y
+        aws --version
+    fi
+
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "This is MacOS"
+    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+    sudo installer -pkg AWSCLIV2.pkg -target /
+
+    elif [[ "$OSTYPE" == "msys" ]]; then
+    echo "Its Windows"
+    msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi
+
+else
+    echo "Sorry I dont know what to do"
+fi
