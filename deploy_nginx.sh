6aab54805e6648c41f9465f5105e6920c741e235
Vadim Aleksandrovich <vadim.alexandrovich@protonmail.ch>
2022-12-20 15:38:28 +0300

    fix:mistake

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -2,7 +2,7 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
-docker login --username <login> --password <password>
-cd httpd && sudo docker build -t vadimalexandrovich/nginx:latest .
-sudo docker run -d -p 8080:8080  -t vadimalexandrovich/nginx:latest
+docker login --username vadimalexandrovich --password nifdaX-syrdak-6cuzhy
+cd nginx && sudo docker build -t vadimalexandrovich/nginx:latest .
+sudo docker run -d -p 80:80  -t vadimalexandrovich/nginx:latest
 docker push vadimalexandrovich/nginx:latest
